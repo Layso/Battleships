895dbf92e920eb5a1984c5d087cf10faafbcb21f
DENİZ CAN ERDEM YILMAZ <canyilmaz1997@gmail.com>
2020-02-16 18:33:48 +0300

    Re-initial commit

--- Assets/Scripts/Global.cs
+++
@@ -1,6 +0,0 @@
-﻿public class Global {
-	public static string[] Languages = { "English", "Turkish" };
-	public static string DEFAULT_LANGUAGE = Languages[0];
-	public static string LANGUAGE_FOLDER_NAME = "Languages";
-	public static string LOG_FILE_NAME = "login.log";
-}
